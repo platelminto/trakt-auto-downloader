790987c08a17d71e0836f41c40945fe775c1c6b1
Giorgio Momigliano <gmomigliano@protonmail.com>
2020-03-03 13:52:43 +0000

    _1377x now actually returns top seeded results from all given searches

--- scrapers/_1377x.py
+++ scrapers/_1377x.py
@@ -80,6 +80,9 @@ def scrape(searches, media_type=MediaType.ANY, options=5, timeout=4):
     if media_type == MediaType.TV_SHOW or media_type == MediaType.SEASON:
         results = [result for result in results if not torrent_is_episode(result.title)]
 
+    results = list(filter(lambda result: result.title != '', results))
+    results.sort(key=lambda result: result.seeders, reverse=True)
+
     return results[:options]
 
 
