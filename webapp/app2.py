a0cf649d0effe73e200f210c31804f4fd0ace6aa
Giorgio Momigliano <gmomigliano@protonmail.com>
2020-03-28 15:37:39 +0100

    Can now add movies by just providing title in webapp

--- webapp/app2.py
+++ webapp/app2.py
@@ -1,7 +1,9 @@
 import configparser
 import datetime
 import os
+import sqlite3
 import sys
+import threading
 from collections import defaultdict
 from threading import Thread
 
@@ -14,7 +16,10 @@ from transmissionrpc import Torrent
 from wtforms import Form, StringField
 from typing import List
 
+from manual_add import get_info
+from media_type import MediaType
 from scrapers.search_result import SearchResult
+from torrent_wrapper import search_torrent, add_magnet, get_torrent_name
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -26,6 +31,8 @@ config.read(os.environ['CONFIG_PATH'])
 
 tmdb.API_KEY = config['TV_SHOWS']['TMDB_API_KEY']
 
+DATABASE_PATH = config['DEFAULT']['DATABASE_PATH']
+
 movie_quality_preference = config['MOVIE_SELECTOR']['QUALITY_PREFERENCE'].replace(' ', '').split(',')
 movie_min_seeders = int(config['MOVIE_SELECTOR']['MINIMUM_SEEDERS'])
 movie_preferred_codec = config['MOVIE_SELECTOR']['PREFERRED_CODEC']
@@ -35,7 +42,7 @@ movie_preferred_codec = config['MOVIE_SELECTOR']['PREFERRED_CODEC']
 current_search_results = list()
 
 
-def auto_select_movie(title: str, search_results: List[SearchResult]):
+def auto_select_movie(title: str, search_results: List[SearchResult]) -> SearchResult:
     title_and_seeder_results = list()
 
     for result in search_results:
@@ -58,6 +65,8 @@ def auto_select_movie(title: str, search_results: List[SearchResult]):
         quality_results[quality].sort(key=lambda result: result.seeders, reverse=True)
         codec_results[quality].sort(key=lambda result: result.seeders, reverse=True)
 
+    picked_movie = None
+
     for quality in movie_quality_preference:
         results = codec_results[quality]
         if len(results) > 0:
@@ -72,17 +81,75 @@ def auto_select_movie(title: str, search_results: List[SearchResult]):
     return picked_movie
 
 
+def pick_movie(result):
+    title = result['title']
+    year = datetime.datetime.strptime(result['release_date'], '%Y-%m-%d').year.numerator
+
+    results = search_torrent([title], MediaType.MOVIE, 25, True)
+    picked_result = auto_select_movie(title, results)
+
+    if not picked_result:
+        return False
+
+    torrent = add_magnet(picked_result.magnet, MediaType.MOVIE)
+
+    t = threading.Thread(target=add_to_movie_db,
+                         args=(torrent, title, year))
+    t.start()
+
+    return True
+
+
+def add_to_movie_db(torrent, name, year):
+    db = sqlite3.connect(DATABASE_PATH)
+    db.cursor().execute(
+        '''INSERT OR IGNORE INTO movie_info 
+           VALUES(?, ?, ?)
+           ''',
+        (get_torrent_name(torrent), name, year))
+    db.commit()
+    db.close()
+
+
 def get_movie(query):
     results = tmdb.Search().movie(query=query)['results'][:8]
-    current_search_results = results
     return results
+
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    title = request.form['autocomplete']
+
+    result = [result for result in current_search_results if result['title'].lower() == title.lower()]
+
+    if not result:
+        return '''
+        <p>Please pick a result from the dropdown</p>
+        '''
+
+    success = pick_movie(result[0])
+
+    if success:
+        return '''
+        <h3>Added {}</h3>
+        <p>To check progress click <a href='http://192.168.1.82:9091/transmission/web/'>here</a></p>
+        <p>To go back and add more, go <a href='http://192.168.1.82:5000/search'>here</a></p>
+        '''.format(title)
+    else:
+        return '''
+        <p>Couldn't find a torrent for {}.</p>
+        <p>Maybe it's too recent?</p>
+        '''
 
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
+    global current_search_results
     try:
         search = request.args.get('q')
-        return jsonify(matching_results=list(get_movie(search)))
+        results = list(get_movie(search))
+        current_search_results = results
+        return jsonify(matching_results=results)
     except Exception as e:
         return str(e)
 
